467bf9a97899dceb2ee1eeece3f57cc9579d3a24
JohnnyL-7 <johnnyleiton1@gmail.com>
2018-01-23 11:47:22 -0500

    cambio de nombre de paquetes

--- app/src/main/java/com/mrclrchtr/android/example/MainActivity.kt
+++ app/src/main/java/com/mrclrchtr/android/example/MainActivity.kt
@@ -8,10 +8,10 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MenuItem
-import com.mrclrchtr.android.example.bar.BarFragment
-import com.mrclrchtr.android.example.baz.BazFragment
-import com.mrclrchtr.android.example.foo.FooFragment
-import com.mrclrchtr.android.example.foobar.FooBarFragment
+import com.mrclrchtr.android.example.bar.Preferencias
+import com.mrclrchtr.android.example.baz.Plan
+import com.mrclrchtr.android.example.foo.Recordatorio
+import com.mrclrchtr.android.example.foobar.Biblioteca
 import com.mrclrchtr.android.example.util.OnFragmentInteractionListener
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar_main.*
@@ -35,10 +35,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val fragmentManager = fragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
 
-        val fooFragment = FooFragment()
+        val fooFragment = Recordatorio()
         fragmentTransaction.add(R.id.frame_layout_main_top, fooFragment)
 
-        val barFragment = BarFragment()
+        val barFragment = Preferencias()
         fragmentTransaction.add(R.id.frame_layout_main_bottom, barFragment)
 
         fragmentTransaction.commit()
@@ -57,16 +57,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_top_foo -> {
-                fragmentManager.beginTransaction().add(R.id.frame_layout_main_top, FooFragment()).commit()
+                fragmentManager.beginTransaction().add(R.id.frame_layout_main_top, Recordatorio()).commit()
             }
             R.id.nav_top_bar -> {
-                fragmentManager.beginTransaction().add(R.id.frame_layout_main_top, BarFragment()).commit()
+                fragmentManager.beginTransaction().add(R.id.frame_layout_main_top, Preferencias()).commit()
             }
             R.id.nav_bottom_foo -> {
-                fragmentManager.beginTransaction().add(R.id.frame_layout_main_bottom, FooFragment()).commit()
+                fragmentManager.beginTransaction().add(R.id.frame_layout_main_bottom, Recordatorio()).commit()
             }
             R.id.nav_bottom_bar -> {
-                fragmentManager.beginTransaction().add(R.id.frame_layout_main_bottom, BarFragment()).commit()
+                fragmentManager.beginTransaction().add(R.id.frame_layout_main_bottom, Preferencias()).commit()
             }
         }
 
@@ -76,10 +76,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onFragmentInteraction(TAG: String, uri: Uri) {
         when (TAG) {
-            BarFragment.TAG -> Log.i(this.TAG, "onFragmentInteraction with BarFragment")
-            FooFragment.TAG -> Log.i(this.TAG, "onFragmentInteraction with FooFragment")
-            BazFragment.TAG -> Log.i(this.TAG, "onFragmentInteraction with BazFragment")
-            FooBarFragment.TAG -> Log.i(this.TAG, "onFragmentInteraction with FooBarFragment")
+            Preferencias.TAG -> Log.i(this.TAG, "onFragmentInteraction with Preferencias")
+            Recordatorio.TAG -> Log.i(this.TAG, "onFragmentInteraction with Recordatorio")
+            Plan.TAG -> Log.i(this.TAG, "onFragmentInteraction with Plan")
+            Biblioteca.TAG -> Log.i(this.TAG, "onFragmentInteraction with Biblioteca")
         }
     }
 }
--- app/src/main/java/com/mrclrchtr/android/example/bar/Preferencias.kt
+++ app/src/main/java/com/mrclrchtr/android/example/bar/Preferencias.kt
@@ -1,4 +1,4 @@
-package com.mrclrchtr.android.example.foobar
+package com.mrclrchtr.android.example.bar
 
 import android.app.Fragment
 import android.content.Context
@@ -16,14 +16,14 @@ import com.mrclrchtr.android.example.util.OnFragmentInteractionListener
  * [OnFragmentInteractionListener] interface
  * to handle interaction events.
  */
-class FooBarFragment : Fragment() {
+class Preferencias : Fragment() {
 
     private var mListener: OnFragmentInteractionListener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_foo_bar, container, false)
+        return inflater.inflate(R.layout.fragment_bar, container, false)
     }
 
     fun onButtonPressed(uri: Uri) {
@@ -47,6 +47,6 @@ class FooBarFragment : Fragment() {
     }
 
     companion object {
-        val TAG = "FooBarFragment"
+        val TAG: String = "Preferencias"
     }
 }
