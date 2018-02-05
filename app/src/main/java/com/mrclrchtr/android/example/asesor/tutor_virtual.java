50e5ca06d08592ecbd44b3f5043eefa91a8b25da
juanmaorpe <juanmanueloetega@gmail.com>
2018-02-05 14:16:50 -0500

    añado mis cambios

---
+++ app/src/main/java/com/mrclrchtr/android/example/asesor/tutor_virtual.java
@@ -0,0 +1,14 @@
+package com.mrclrchtr.android.example.asesor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mrclrchtr.android.example.R;
+
+public class tutor_virtual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_virtual);
+    }
+}
