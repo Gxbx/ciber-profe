8a50ae49fe5666de9b444ee004889a82120c4954
juanmaorpe <juanmanueloetega@gmail.com>
2018-02-19 14:44:17 -0500

    casi vumetro

--- app/src/main/java/com/mrclrchtr/android/example/asesor/tutor_virtual.java
+++ app/src/main/java/com/mrclrchtr/android/example/asesor/tutor_virtual.java
@@ -3,6 +3,10 @@ package com.mrclrchtr.android.example.asesor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.mrclrchtr.android.example.R;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.res.Resources;
+import android.media.AudioFormat;
 
 public class tutor_virtual extends AppCompatActivity {
 
